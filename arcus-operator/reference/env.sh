5e5fdc6f41371f181c8f462a28a652cb991d9fc5
hjyun <hjyun@jam2in.com>
2019-02-11 18:56:35 +0900

    CLEANUP: remove reference folder

--- arcus-operator/reference/env.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-DOMAIN=example-zookeepercluster-headless.default.svc.cluster.local
-QUORUM_PORT=2888
-LEADER_PORT=3888
-CLIENT_HOST=example-zookeepercluster-client
-CLIENT_PORT=2181
